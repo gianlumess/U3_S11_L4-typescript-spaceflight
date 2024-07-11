a833ff7319b0712e724337e393aff95c13b04147
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-07-11 16:55:37 +0200

    creata detailPage dinamica

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
-const Detail = (props) => {
-  const [articleDetail, setArticleDetail] = useState([]);
+import TopBar from "../components/TopBar";
+import { Col, Container, Image, Row } from "react-bootstrap";
+import { IArticle } from "../interfaces/IArticle";
+interface Obj {
+  detail: IArticle;
+}
+const Detail = () => {
+  const [articleDetail, setArticleDetail] = useState({});
   const params = useParams();
 
   const fetchArticleDetail = async () => {
@@ -22,7 +27,25 @@ const Detail = (props) => {
   useEffect(() => {
     fetchArticleDetail();
   }, []);
-  return <></>;
+
+  return (
+    <>
+      <TopBar />
+      <Container className="border mt-5 p-2">
+        <Row>
+          <Col md={6}>
+            <Image className="w-100" src={articleDetail.image_url} />
+          </Col>
+
+          <Col>
+            <h1>{articleDetail.title}</h1>
+            <p>{articleDetail.summary}</p>
+            <span className="badge">{articleDetail.published_at}</span>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default Detail;
